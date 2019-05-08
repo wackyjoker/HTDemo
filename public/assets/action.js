4541751da682651140ecde7c2ccaa9f58e6301f9
wackyjoker <kevin.young8931@gmail.com>
2019-05-08 16:52:05 +0200

    hot fix

--- public/assets/action.js
+++ public/assets/action.js
@@ -1,4 +1,49 @@
 //AJAX fetching from bacon ipsum
+$(document).ready( function() {
+
+  loadPopupBox();
+
+  $('#popupBoxClose').click( function() {            
+      unloadPopupBox();
+  });
+
+  $('#container').click( function() {
+      unloadPopupBox();
+  });
+
+  function unloadPopupBox() {    // TO Unload the Popupbox
+      $('#popup_box').fadeOut("slow");
+      $("#container").css({ // this is just for style        
+          "opacity": "1"  
+      }); 
+  }    
+
+  function loadPopupBox() {    // To Load the Popupbox
+
+      var counter = 10;
+      var id;
+      $('#popup_box').fadeIn("slow");
+      $("#container").css({ // this is just for style
+          "opacity": "0.3"  
+      });
+
+      id = setInterval(function() {
+          counter--;
+          if(counter < 0) {
+              clearInterval(id);
+
+              unloadPopupBox();
+          } else {
+              $("#countDown").text("it closed  after " + counter.toString() + " seconds.");
+          }
+      }, 1000);
+
+  }        
+});
+
+
+
+
 
 	var url = "https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1";
 	var s1_h3 = document.getElementById("s1-h3");
